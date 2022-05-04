cb72c7dd455709406e8182d57dd19c3c0571b1ce
Alla Smelkova <87067294+Smelkovaalla@users.noreply.github.com>
2022-05-04 22:06:03 +0300

    2 commit

--- 2.1_index.js
+++ 2.1_index.js
@@ -1,20 +1,22 @@
-function simple(num) {
-    var list = new Array;
-
+function simple(am) {
+    const list = new Array;
+    let num = 2;
     for (let i = 2; i <= num; i++) {
         for (let j = 2; j <= i; j++) { 
-            if ((i % j == 0) && (j != i)) {break;} else {list.push(i);;break;};
+            if ((i % j == 0) && (j != i)) {
+                num = num + 1;
+                break;
+            }else{
+                list.push(i);
+                if (list.length < am) {
+                    num = num + 1;
+                }
+                break;};
         };
     };
-
-    var string = '';
-    if(num > 0) {
-    for (var i = 0; i < list.length; i++) {
-        string = string + list[i] + ' ';
-    };
-    };
-
-    return string;
+    return list;
 }
 
 console.log(simple(+process.argv[2]));
+
+// node 2.1_index 9
